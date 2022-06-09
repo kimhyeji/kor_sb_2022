fd7d1df63a9eb356bdbef1a28efeb1bb9c309235
kimhyeji <gpwl5589@naver.com>
2022-06-09 21:25:47 +0900

    getArticle 액션 구현

--- src/main/java/com/khj/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/khj/exam/demo/controller/UsrArticleController.java
@@ -43,6 +43,7 @@ public class UsrArticleController {
 		
 		return article;
 	}
+	
 	private Article getArticle(int id) {
 		for ( Article article : articles ) {
 			if ( article.getId() == id ) {
@@ -79,6 +80,18 @@ public class UsrArticleController {
 	@ResponseBody
 	public List<Article> getArticles() {
 		return articles;
+	}
+	
+	@RequestMapping("/usr/article/getArticle")
+	@ResponseBody
+	public Object getArticleAction(int id) {
+		Article article = getArticle(id);
+		
+		if ( article == null) {
+			return id + "번 게시물은 존재하지 않습니다.";
+		}
+		
+		return article;
 	}
 	
 	@RequestMapping("/usr/article/doDelete")
