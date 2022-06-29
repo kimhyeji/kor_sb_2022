5ad0313cdbc81442def7194e70abdf0b9132bfa5
kimhyeji <gpwl5589@naver.com>
2022-06-29 21:27:22 +0900

    회원가입 완료 후, 생성된 회원 보여줌

--- src/main/java/com/khj/exam/demo/service/MemberService.java
+++ src/main/java/com/khj/exam/demo/service/MemberService.java
@@ -3,6 +3,7 @@ package com.khj.exam.demo.service;
 import org.springframework.stereotype.Service;
 
 import com.khj.exam.demo.repository.MemberRepository;
+import com.khj.exam.demo.vo.Member;
 
 @Service
 public class MemberService {
@@ -12,8 +13,13 @@ public class MemberService {
 		this.memberRepository = memberRepository;
 	}
 
-	public void join(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
+	public int join(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
 		memberRepository.join(loginId, loginPw, name, nickname, cellphoneNo, email);
+		return memberRepository.getLastInsertId();
+	}
+
+	public Member getMemberById(int id) {
+		return memberRepository.getMemberById(id);
 	}
 
 }
