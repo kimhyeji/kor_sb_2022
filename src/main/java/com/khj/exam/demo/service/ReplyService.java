1deb5f34039b2f3574039f3965e17a897d7e355b
kimhyeji <gpwl5589@gmail.com>
2022-07-29 20:32:33 +0900

    댓글 작성 처리

---
+++ src/main/java/com/khj/exam/demo/service/ReplyService.java
@@ -0,0 +1,26 @@
+package com.khj.exam.demo.service;
+
+import org.springframework.stereotype.Service;
+
+import com.khj.exam.demo.repository.ReplyRepository;
+import com.khj.exam.demo.utill.Ut;
+import com.khj.exam.demo.vo.ResultData;
+import com.khj.exam.demo.vo.Rq;
+
+@Service
+public class ReplyService {
+	private ReplyRepository replyRepository;
+	private Rq rq;
+
+	public ReplyService(ReplyRepository replyRepository, Rq rq) {
+		this.replyRepository = replyRepository;
+		this.rq = rq;
+	}
+
+	public ResultData<Integer> writeReply(int actorId, String relTypeCode, int relId, String body) {
+		replyRepository.writeReply(actorId, relTypeCode, relId, body);
+		int id = replyRepository.getLastInsertId();
+		
+		return ResultData.from("S-1", Ut.f("%d번 댓글이 생성되었습니다.", id), "id", id);
+	}
+}
