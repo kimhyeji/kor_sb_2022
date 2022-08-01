04a01b917f0c17ee9833d63efb86848688492c11
kimhyeji <gpwl5589@gmail.com>
2022-08-01 20:33:15 +0900

    댓글 삭제

--- src/main/java/com/khj/exam/demo/repository/ReplyRepository.java
+++ src/main/java/com/khj/exam/demo/repository/ReplyRepository.java
@@ -2,6 +2,7 @@ package com.khj.exam.demo.repository;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
@@ -38,5 +39,28 @@ public interface ReplyRepository {
 			ORDER BY R.id DESC
 			""")
 	public List<Reply> getForPrintReplies(int memberId, String relTypeCode, int relId);
+
+	@Select("""
+			SELECT R.*,
+			M.nickname AS extra__writerName
+			FROM reply As R
+			LEFT JOIN `member` AS M
+			ON R.memberId = M.id
+			WHERE R.id = #{id}
+			""")
+	public Reply getForPrintReply(int memberId, int id);
+	
+	@Select("""
+			SELECT R.*
+			FROM reply As R
+			WHERE R.id = #{id}
+			""")
+	public Reply getReply(int id);
+
+	@Delete("""
+			DELETE FROM reply
+			WHERE id = #{id}
+			""")
+	public void deleteReply(int id);
 
 }
