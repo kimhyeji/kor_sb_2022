e288648a197087c5985ad86f2b30ccd50c4500ca
kimhyeji <gpwl5589@gmail.com>
2022-08-03 19:36:36 +0900

    회원정보 수정 전에, 비밀번호 체크시 인증코드 생성

--- src/main/java/com/khj/exam/demo/service/MemberService.java
+++ src/main/java/com/khj/exam/demo/service/MemberService.java
@@ -10,8 +10,10 @@ import com.khj.exam.demo.vo.ResultData;
 @Service
 public class MemberService {
 	private MemberRepository memberRepository;
+	private AttrService attrService;
 
-	public MemberService(MemberRepository memberRepository) {
+	public MemberService(AttrService attrService, MemberRepository memberRepository) {
+		this.attrService = attrService; 
 		this.memberRepository = memberRepository;
 	}
 
@@ -53,6 +55,13 @@ public class MemberService {
 		memberRepository.modify(loginedMemberId, loginPw, name, nickname, email, cellphoneNo);
 		
 		return ResultData.from("S-1", "회원정보가 수정되었습니다.");
+	}
+
+	public String genMemberModifyAuthKey(int actorId) {
+		String memberModifyAuthKey = Ut.getTempPassword(10);
+		
+		attrService.setValue("member", actorId, "extra", "memberModifyAuthKey", memberModifyAuthKey, Ut.getDateStrLater(60 * 5));
+		return memberModifyAuthKey;
 	}
 
 }
